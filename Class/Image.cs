6e473ad7e8a3da387c44193b0e0b49df6b9a2375
Robin van de Griend <robinvandegriend@gmail.com>
2019-09-24 16:08:45 +0200

    Create Image class with constructor

---
+++ Class/Image.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace INFOIBV
+{
+    class Image
+    {
+        // An Image is an 2 dimensional array of greyscale pixel values lying in the interval [0,255]
+        // It provides constructor methods to create an Image from a System.Drawing.BitMap object
+        // Image operations such as morphological filters are implemented as instance methods
+        // as they are "things you can do to this image"
+
+        public int Width;
+        public int Height;
+        int[,] PixelValues;
+
+        public Image(Bitmap bitmap)
+        {
+            Width = bitmap.Size.Width;
+            Height = bitmap.Size.Height;
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    PixelValues[x,y] = bitmap.GetPixel(x, y).GetGreyscaleValue();
+                }
+            }
+        }
+    }
+}
---
+++ Extension/ColorExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace INFOIBV
+{
+    public static class ColorExtension
+    {
+        public static int GetGreyscaleValue(this Color color)
+        {
+            var r = color.R;
+            var g = color.G;
+            var b = color.B;
+
+            if (r != b | b != g)
+            {
+                throw new ArgumentException("Image is not greyscale");
+            }
+            return r;
+        }
+    }
+}
