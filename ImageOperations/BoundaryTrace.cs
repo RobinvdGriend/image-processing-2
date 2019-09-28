fa3039659707c2bea5c843553c27e1378acc3915
Sam Beard <sam.beard@zelezny.nl>
2019-09-28 14:12:46 +0200

    implement  boundary trace

--- ImageOperations/BoundaryTrace.cs
+++ ImageOperations/BoundaryTrace.cs
@@ -9,6 +9,16 @@ using System.Windows.Forms;
 
 namespace INFOIBV.ImageOperations
 {
+    enum Direction {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest
+    }
     class BoundaryTrace : IImageOperation
     {
         private StructuringElement _se;
@@ -23,7 +33,61 @@ namespace INFOIBV.ImageOperations
             p2.Erode(_se);
             p2.Complement();
             p.And(p2);
+            var a = BoundaryPixels(p);
             return p.GetBitmap();
+        }
+
+        private List<Point> BoundaryPixels(BinaryProcessor p) {
+            var dirs = new List<Tuple<Direction, Point>>() {
+                new Tuple<Direction,Point>(Direction.North, new Point(0,-1)),
+                new Tuple<Direction,Point>(Direction.NorthEast, new Point(1,-1) ),
+                new Tuple<Direction, Point>(Direction.East, new Point(1,0) ),
+                new Tuple<Direction, Point>(Direction.SouthEast, new Point(1,1) ),
+                new Tuple<Direction, Point>(Direction.South, new Point(0,1) ),
+                new Tuple<Direction, Point>(Direction.SouthWest, new Point(-1,1) ),
+                new Tuple<Direction, Point>(Direction.West, new Point(-1,0) ),
+                new Tuple<Direction, Point>(Direction.NorthWest, new Point(-1,-1) ),
+            };
+
+            var point = findFirstPoint(p);
+            if (point.X == -1)
+                return null;
+
+            var set = new HashSet<Tuple<Point, Direction>>();
+            var list = new List<Point>();
+            var img = p.Image;
+            while (true){
+                Tuple<Direction, Point> dir = null;
+                foreach (var d in dirs)
+                {
+                    dir = d;
+                    if (img[d.Item2.X + point.X, d.Item2.Y + point.Y] > 0)
+                        break;
+                }
+                var entry = new Tuple<Point, Direction>(point, dir.Item1);
+                if (set.Contains(entry))
+                    break;
+                else {
+                    set.Add(entry);
+                    var pnt = dir.Item2;
+                    list.Add(entry.Item1);
+                    point = new Point(point.X + pnt.X, point.Y + pnt.Y);
+                }
+            }
+            return list;
+        }
+
+        private Point findFirstPoint(BinaryProcessor p)
+        {
+            for (int x = 0; x < p.Width; x++)
+            {
+                for (int y = 0; y < p.Height; y++)
+                {
+                    if (p.Image[x, y] > 0)
+                        return new Point(x, y);
+                }
+            }
+            return new Point(-1, -1);
         }
 
         public override string ToString()
